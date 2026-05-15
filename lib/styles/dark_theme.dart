a235ff70a22b1fb0407eb1611d9bf4b45746cebb
GamePad <hejdjdjuduf@gmail.com>
2026-05-15 10:42:00 +0300

    feat: add colors for h1,h2... in markdown

--- lib/styles/dark_theme.dart
+++ lib/styles/dark_theme.dart
@@ -19,10 +19,13 @@ class DarkTheme {
   static final appBarColor = const Color.fromARGB(255, 44, 44, 44);
 
   static final markdownTheme = MarkdownStyleSheet(
-    a: TextStyle(color: Colors.white),
-    h1: TextStyle(color: Colors.white),
-    h2: TextStyle(color: Colors.white),
-    h3: TextStyle(color: Colors.white),
+    a: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+    h1: TextStyle(color: Color.fromARGB(255, 77, 106, 255)),
+    h2: TextStyle(color: Color.fromARGB(255, 77, 106, 255)),
+    h3: TextStyle(color: Color.fromARGB(255, 77, 106, 255)),
+    h4: TextStyle(color: Color.fromARGB(255, 77, 106, 255)),
+    h5: TextStyle(color: Color.fromARGB(255, 77, 106, 255)),
+    h6: TextStyle(color: Color.fromARGB(255, 77, 106, 255)),
     p: TextStyle(color: Colors.white),
     listBullet: TextStyle(color: Colors.white),
     code: TextStyle(color: Colors.white, backgroundColor: Colors.grey[800])
